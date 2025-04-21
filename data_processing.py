e980bf3ca24d7b94529c3763fd4777ab1dd6b526
meelunae <git@eleuna.me>
2025-04-21 03:01:26 +0200

    Split output of data processing in two collections

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import hashlib
+import csv
 
 # Load the dataset
 dataset_path = "./dataset/taylorswift-Tracks.csv"
@@ -32,12 +34,15 @@ filtered_albums = []
 
 for album in album_priority:
     if "Taylor's Version" in album:
-        base_name = album.replace(" [Deluxe]", "")  # Prefer Deluxe if available
+        # Always include Taylor's Version albums
+        filtered_albums.append(album)
+
+        # Update base_albums only if it's not already a Deluxe version or this is a Deluxe
+        base_name = album.replace(" [Deluxe]", "")
         if base_name not in base_albums or "[Deluxe]" in album:
             base_albums[base_name] = album
-            filtered_albums.append(album)
     else:
-        base_name = album.split(" (")[0]  # Extract base album name ignoring editions
+        base_name = album.split(" (")[0]
         if base_name not in base_albums:
             base_albums[base_name] = album
             filtered_albums.append(album)
@@ -59,3 +64,61 @@ df_filtered = df_filtered.sort_values(by=["release_date", "track_number"], ascen
 
 # Save cleaned dataset
 df_filtered.to_csv("./dataset/taylorswift-Tracks-cleaned.csv", index=False)
+
+
+
+albums = {}
+for _, row in df_filtered.iterrows():
+    album_name = row['album']
+    release_date = row['release_date']
+
+    album_key = f"{album_name}_{release_date}"
+    album_id = hashlib.md5(album_key.encode()).hexdigest()
+
+    if album_id not in albums:
+        albums[album_id] = {
+            "_id": album_id,
+            "name": album_name,
+            "release_date": release_date
+        }
+
+tracks = []
+for _, row in df_filtered.iterrows():
+    album_key = f"{row['album']}_{row['release_date']}"
+    album_id = hashlib.md5(album_key.encode()).hexdigest()
+
+    track = {
+        "id": row["id"],
+        "name": row["name"],
+        "track_number": row["track_number"],
+        "uri": row["uri"],
+        "acousticness": row["acousticness"],
+        "danceability": row["danceability"],
+        "energy": row["energy"],
+        "instrumentalness": row["instrumentalness"],
+        "liveness": row["liveness"],
+        "loudness": row["loudness"],
+        "speechiness": row["speechiness"],
+        "tempo": row["tempo"],
+        "valence": row["valence"],
+        "popularity": row["popularity"],
+        "duration_ms": row["duration_ms"],
+        "album_id": album_id
+    }
+    tracks.append(track)
+
+print("Albums collection:")
+print(list(albums.values())[:2])  # Preview first 2 albums
+print("\nTracks collection:")
+print(tracks[:2])  # Preview first 2 tracks
+
+with open("./dataset/albums.csv", mode="w", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=["_id", "name", "release_date"])
+    writer.writeheader()
+    writer.writerows(albums.values())
+fieldnames = tracks[0].keys()
+
+with open("./dataset/tracks.csv", mode="w", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(tracks)
